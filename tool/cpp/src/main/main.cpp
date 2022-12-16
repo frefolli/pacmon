d163bd16db9c784cda1a2d555f7d6d2f9a102d11
frefolli <f.refolli@campus.unimib.it>
2022-12-16 16:01:39 +0100

    amogus

--- tool/cpp/src/main/main.cpp
+++ tool/cpp/src/main/main.cpp
@@ -13,15 +13,15 @@ template<class Root, class Platform, class Package, class Version>
 void someTree(std::string path) {
   Root* root = new Root(path);
   println(root->toString());
-  for (int i = 0; i < root->getNumberOfPlatforms(); i++) {
+  for (long unsigned int i = 0; i < root->getNumberOfPlatforms(); i++) {
     std::string platformName = root->getPlatform(i);
     Platform* platform = new Platform(path + "/" + platformName);
     println(platform->toString());
-    for (int j = 0; j < platform->getNumberOfPackages(); j++) {
+    for (long unsigned int j = 0; j < platform->getNumberOfPackages(); j++) {
       std::string packageName = platform->getPackage(j);
       Package* package = new Package(path + "/" + platformName + "/" + packageName);
       println(package->toString());
-      for (int k = 0; k < package->getNumberOfVersions(); k++) {
+      for (long unsigned int k = 0; k < package->getNumberOfVersions(); k++) {
 	lib::types::Semver semver = package->getVersion(k);
 	Version* version = new Version(path + "/" + platformName + "/" + packageName + "/" + semver.toString());
 	println(version->toString());
@@ -40,7 +40,7 @@ int main(int argc, char** args) {
     if (argc > 1) {
       path = std::filesystem::absolute(std::filesystem::path(std::string(args[1]))).string();
       if (! std::filesystem::exists(std::filesystem::path(path)))
-	throw new std::runtime_error("folder <" + path + "> passed as argument doesn't exists");
+	    throw std::runtime_error("folder <" + path + "> passed as argument doesn't exists");
     }
 
     someTree<lib::source::Source, lib::source::Platform,
@@ -50,7 +50,7 @@ int main(int argc, char** args) {
 	     lib::repository::Package, lib::repository::Version>
       (path + "/repository");
   
-  } catch(std::runtime_error error) {
+  } catch(std::runtime_error& error) {
     println((&error)->what());
   }
 }
